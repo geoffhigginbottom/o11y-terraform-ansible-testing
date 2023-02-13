d978e6adb1f5032a872012ae1ab2200c4b7929ca
Geoff Higginbottom <github@geoffh.co.uk>
2023-02-13 12:51:22 +0000

    Renamed File

--- modules/instances/ansible-install-otel-agent.tf
+++ modules/instances/ansible-install-otel-agent.tf
@@ -4,6 +4,7 @@ resource "local_file" "otel_agent_hosts" {
     {
       mysql_servers = aws_instance.mysql.*.public_ip
       ubuntu_servers = aws_instance.ubuntu.*.public_ip
+      windows_servers = aws_instance.windows_server.*.public_ip
     }
   )
   filename = "${path.module}/otel_agent_hosts"
@@ -13,9 +14,12 @@ resource "null_resource" "otel_agent_hosts" {
   
   triggers = {
     mysql_servers = join(",", aws_instance.mysql.*.public_ip) # will trigger ansible whenever there is a change to the list of ips for mysql servers 
-    ubuntu_servers = join(",", aws_instance.ubuntu.*.public_ip) # will trigger ansible whenever there is a change to the list of ips for ubuntu servers 
+    ubuntu_servers = join(",", aws_instance.ubuntu.*.public_ip) # will trigger ansible whenever there is a change to the list of ips for ubuntu servers
+    windows_servers = join(",", aws_instance.windows_server.*.public_ip) # will trigger ansible whenever there is a change to the list of ips for windows servers 
     force_run_ansible_install_otel_agent = var.force_run_ansible_install_otel_agent ? "${timestamp()}" : null # will trigger ansible if var.force_run_ansible_install_otel_agent is set to true - located in terraform.tfvars
   }
+
+### Triggers the ansible role otel-agent to install the otel agent on hosts using either ansible-galaxy or a local playbook version depending en the setting of 'galaxy_otel' in terafrom.tfvars
 
   provisioner "local-exec" {
     command = "ansible-playbook -i ${path.module}/otel_agent_hosts ${path.module}/otel-agent.yml --extra-vars 'realm=${var.realm} accesstoken=${var.access_token} galaxy_otel=${var.galaxy_otel}'"
