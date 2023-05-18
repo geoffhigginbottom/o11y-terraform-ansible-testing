743a24c696f1b22ce1eebae9e240ad0bb335ab72
Geoff Higginbottom <github@geoffh.co.uk>
2023-05-18 14:59:07 +0100

    Update ansible-install-otel-agent.tf

--- modules/instances/ansible-install-otel-agent.tf
+++ modules/instances/ansible-install-otel-agent.tf
@@ -1,10 +1,13 @@
 ### Generate Inventory file for Ansible ###
 resource "local_file" "otel_agent_hosts" {
-  content = templatefile("${path.module}/templates/otel_agent_hosts",
+  content = templatefile("${path.module}/templates/otel_agent_hosts.tftpl",
     {
       mysql_servers = aws_instance.mysql.*.public_ip
       ubuntu_servers = aws_instance.ubuntu.*.public_ip
       windows_servers = aws_instance.windows_server.*.public_ip
+      windows_server_administrator_pwd = var.windows_server_administrator_pwd
+      access_token = var.access_token
+      realm = var.realm
     }
   )
   filename = "${path.module}/otel_agent_hosts"
@@ -22,6 +25,6 @@ resource "null_resource" "otel_agent_hosts" {
 ### Triggers the ansible role otel-agent to install the otel agent on hosts using either ansible-galaxy or a local playbook version depending en the setting of 'galaxy_otel' in terafrom.tfvars
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${path.module}/otel_agent_hosts ${path.module}/otel-agent.yml --extra-vars 'realm=${var.realm} accesstoken=${var.access_token} galaxy_otel=${var.galaxy_otel}'"
+    command = "ansible-playbook -i ${path.module}/otel_agent_hosts ${path.module}/ansible_role_otel-agent.yaml --extra-vars 'realm=${var.realm} accesstoken=${var.access_token} galaxy_otel=${var.galaxy_otel}'"
   }
 }
