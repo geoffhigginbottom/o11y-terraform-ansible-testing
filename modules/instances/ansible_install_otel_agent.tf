12eadf35400adb6609c0aeb09cb18f5102982e55
Geoff Higginbottom <github@geoffh.co.uk>
2023-08-30 17:55:36 +0100

    standardised naming convention with underscores

--- modules/instances/ansible_install_otel_agent.tf
+++ modules/instances/ansible_install_otel_agent.tf
@@ -26,6 +26,6 @@ resource "null_resource" "otel_agent_hosts" {
 ### Triggers the ansible role otel-agent to install the otel agent on hosts using either ansible-galaxy or a local playbook version depending en the setting of 'galaxy_otel' in terafrom.tfvars
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${path.module}/otel_agent_hosts ${path.module}/ansible_role_otel-agent.yaml --extra-vars 'realm=${var.realm} accesstoken=${var.access_token} galaxy_otel=${var.galaxy_otel}'"
+    command = "ansible-playbook -i ${path.module}/otel_agent_hosts ${path.module}/ansible_role_otel_agent.yaml --extra-vars 'realm=${var.realm} accesstoken=${var.access_token} galaxy_otel=${var.galaxy_otel}'"
   }
 }
